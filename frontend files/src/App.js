fc32315f94c0c3a5cd9d9d0445fd6a2d6473b30a
sonalr468-gsn <sonalr468@gmail.com>
2025-11-05 19:07:28 -0800

    Update App.js

--- frontend files/src/App.js
+++ frontend files/src/App.js
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import SignupPage from "./SignupPage";
 import DashboardPage from "./DashboardPage";
 import EventPage from "./EventPage";
-import LoginPage from "./LoginPage";
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/events" element={<EventPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
-export default App;
 
 
